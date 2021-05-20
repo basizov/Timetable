44886d94761c95fe73d47f644ffe869ac83e271a
boris <borang.3006@yandex.ru>
2021-05-20 23:17:48 +0300

    Server error
    Были обработаны ошибки сервера.

--- client-app/src/components/ServerError/ServerError.tsx
+++ client-app/src/components/ServerError/ServerError.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { store } from '../../app/stores/store';
+import server from '../../assets/server.svg';
 
 const ServerError: React.FC = () => {
   return (
     <section className="server">
       <div className="server__wrapper">
-        
+        <div className="server__left">
+          <div className="server__header">
+            <h2 className="server__code">{store.commonStore.error?.statusCode ?? 500}</h2>
+            <h3 className="server__message">Ошибка сервера</h3>
+          </div>
+          <Link to="/" className="link server__link">Перейти на главную страницу</Link>
+          <div className="server__details">{store.commonStore.error?.message ?? 'Внутрення ошибка сервера'}</div>
+        </div>
+        <div className="server__right">
+          <img src={server} alt="server" />
+        </div>
       </div>
     </section>
   );
